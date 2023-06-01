c15d1a73a53b961298d65ceb78d570ec823a5b35
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-06-01 09:49:45 +0530

    check isNumber

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var readline_sync_1 = require("readline-sync");
+function main() {
+    // console.log(question)
+    var num1 = (0, readline_sync_1.question)("Enter the first number: \n");
+    var op = (0, readline_sync_1.question)("Enter the Operation: \n");
+    var num2 = (0, readline_sync_1.question)("Enter the second number: \n");
+    console.log(num1, op, num2);
+    var firstNum = isNumber(num1);
+    console.log(firstNum);
+}
+function isNumber(str) {
+    var maybenum = parseInt(str);
+    var isNum = !isNaN(maybenum);
+    return isNum;
+}
+main();
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("hello")
+import { question } from "readline-sync";
+
+function main(): void {
+  // console.log(question)
+  const num1: string = question("Enter the first number: \n");
+  const op: string = question("Enter the Operation: \n");
+  const num2: string = question("Enter the second number: \n");
+  console.log(num1, op, num2);
+
+  const firstNum = isNumber(num1);
+  console.log(firstNum)
+}
+
+function isNumber(str: string) {
+    const maybenum = parseInt(str)
+    const isNum:boolean = !isNaN(maybenum)
+    return isNum
+}
+
+main();
