3d55917354c81d87142e6c42e4a97b747ee2d63f
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-06-01 10:18:23 +0530

    done ClI calculator

--- index.js
+++ index.js
@@ -2,27 +2,48 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var readline_sync_1 = require("readline-sync");
 function main() {
-    // console.log(question)
+    //Get Input
     var num1 = (0, readline_sync_1.question)("Enter the first number: \n");
     var op = (0, readline_sync_1.question)("Enter the Operation: \n");
     var num2 = (0, readline_sync_1.question)("Enter the second number: \n");
-    console.log(num1, op, num2);
-    var firstNum = isNumber(num1);
-    console.log(firstNum);
-    var isOp = isOperater(op);
-    console.log(isOp);
+    //check for vaild input
+    var isVaild = isNumber(num1) && isOperator(op) && isNumber(num2);
+    if (isVaild) {
+        var firstNum = parseInt(num1);
+        var secondNum = parseInt(num2);
+        var result = calculator(firstNum, op, secondNum);
+        console.log("Result :", result);
+    }
+    else {
+        console.log("\nInvaild input \n");
+        main();
+    }
 }
-function isOperater(Operater) {
+function calculator(firstNum, op, secondNum) {
+    switch (op) {
+        case "+":
+            return firstNum + secondNum;
+        case "-":
+            return firstNum - secondNum;
+        case "*":
+            return firstNum * secondNum;
+        case "/":
+            return firstNum / secondNum;
+    }
+}
+// check for vaild operators
+function isOperator(Operater) {
     switch (Operater) {
-        case '+':
-        case '-':
-        case '*':
-        case '/':
+        case "+":
+        case "-":
+        case "*":
+        case "/":
             return true;
         default:
             return false;
     }
 }
+// check for vaild number
 function isNumber(str) {
     var maybenum = parseInt(str);
     var isNum = !isNaN(maybenum);
--- index.ts
+++ index.ts
@@ -1,36 +1,56 @@
 import { question } from "readline-sync";
 
+type Operater = "+" | "-" | "*" | "/";
+
 function main(): void {
-  // console.log(question)
+  //Get Input
   const num1: string = question("Enter the first number: \n");
   const op: string = question("Enter the Operation: \n");
   const num2: string = question("Enter the second number: \n");
-  console.log(num1, op, num2);
 
-  const firstNum = isNumber(num1);
-  console.log(firstNum)
-
-  const isOp = isOperater (op)
-  console.log(isOp)
+  //check for vaild input
+  const isVaild: boolean = isNumber(num1) && isOperator(op) && isNumber(num2);
+  if (isVaild) {
+    const firstNum: number = parseInt(num1);
+    const secondNum: number = parseInt(num2);
+    const result = calculator(firstNum, op as Operater, secondNum);
+    console.log("Result :", result);
+  } else {
+    console.log("\nInvaild input \n");
+    main();
+  }
 }
 
-function isOperater(Operater:string):boolean{
-    switch(Operater){
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            return true 
-        default: 
-            return false    
-    }
-
+function calculator(firstNum: number, op: Operater, secondNum: number) {
+  switch (op) {
+    case "+":
+      return firstNum + secondNum;
+    case "-":
+      return firstNum - secondNum;
+    case "*":
+      return firstNum * secondNum;
+    case "/":
+      return firstNum / secondNum;
+  }
+}
+// check for vaild operators
+function isOperator(Operater: string): boolean {
+  switch (Operater) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      return true;
+    default:
+      return false;
+  }
 }
 
+// check for vaild number
 function isNumber(str: string) {
-    const maybenum = parseInt(str)
-    const isNum:boolean = !isNaN(maybenum)
-    return isNum
+  const maybenum = parseInt(str);
+  const isNum: boolean = !isNaN(maybenum);
+  return isNum;
 }
 
 main();
